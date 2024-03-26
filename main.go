314cf6db330853f01b9c13589821c123260c2687
ICHINOSE Shogo <shogo82148@gmail.com>
2024-03-27 00:15:15 +0900

    multi process

--- main.go
+++ main.go
@@ -2,24 +2,32 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"crypto/ed25519"
 	"encoding/base64"
 	"encoding/pem"
+	"fmt"
 	"io"
 	"log"
 	"math/rand"
 	"os"
+	"os/exec"
 	"runtime"
+	"strconv"
+	"sync"
 
 	"go.step.sm/crypto/pemutil"
 	"golang.org/x/crypto/ssh"
 )
 
+const ChildEnvName = "BRUTE_SSH_KEY_CHILD"
+const TotalEnvName = "BRUTE_SSH_KEY_TOTAL"
+
 var (
 	aKey    []byte = []byte("a")
 	aKeyLen int    = 1
 
-	target []byte = []byte("AAAAC3NzaC1lZDI1NTE5AAAAIBVji5kBXa8PbDP1nk+nysVA89VMg27z98D4aVT/j4Fa")
+	target = []byte("AAAAC3NzaC1lZDI1NTE5AAAAIBVji5kBXa8PbDP1nk+nysVA89VMg27z98D4aVT/j4Fa")
 )
 
 const (
@@ -37,8 +45,8 @@ func generateKey(rand io.Reader, seed [ed25519.SeedSize]byte) ed25519.PrivateKey
 	return ed25519.NewKeyFromSeed(seed[:])
 }
 
-func bruteAuthorizedKey(privateKeyChan chan<- ed25519.PrivateKey, idx, total int) {
-	for i := idx; i < 1<<32; i += total {
+func bruteAuthorizedKey(idx, total int) ed25519.PrivateKey {
+	for i, j := idx, 0; i < 1<<32; i, j = i+total, j+1 {
 		fastRandReader := &fastRandReaderImpl{rand.New(rand.NewSource(int64(i)))}
 		var seed [ed25519.SeedSize]byte
 		var encodedPublicKey [encodedPublicKeyLen]byte
@@ -51,27 +59,83 @@ func bruteAuthorizedKey(privateKeyChan chan<- ed25519.PrivateKey, idx, total int
 			base64.StdEncoding.Encode(encodedPublicKey[:], privateKey[32-1:])
 
 			if bytes.Equal(encodedPublicKey[encodedPublicKeyLen-aKeyLen:], aKey) {
-				if i%2048 == 0 {
+				if j%1024 == 0 {
 					log.Printf("%08x %s", i, string(encodedPublicKey[:]))
 				}
 				if bytes.Equal(encodedPublicKey[2:], target[len(target)-encodedPublicKeyLen+2:]) {
-					privateKeyChan <- privateKey
+					return privateKey
 				}
 				break
 			}
 		}
 	}
+	return nil
+}
+
+func childProcess() {
+	idx, err := strconv.Atoi(os.Getenv(ChildEnvName))
+	if err != nil {
+		panic(err)
+	}
+	total, err := strconv.Atoi(os.Getenv(TotalEnvName))
+	if err != nil {
+		panic(err)
+	}
+	privateKey := bruteAuthorizedKey(idx, total)
+	if privateKey == nil {
+		return
+	}
+
+	pemBlock, _ := pemutil.SerializeOpenSSHPrivateKey(privateKey)
+	pem.Encode(os.Stdout, pemBlock)
+}
+
+func child(ctx context.Context, privateKeyChan chan<- ed25519.PrivateKey, i int, total int) {
+	cmd := exec.CommandContext(ctx, os.Args[0], os.Args[1:]...)
+	cmd.Env = append(
+		os.Environ(),
+		fmt.Sprintf("%s=%d", ChildEnvName, i),
+		fmt.Sprintf("%s=%d", TotalEnvName, total),
+	)
+	var buf bytes.Buffer
+	cmd.Stdout = &buf
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return
+	}
+
+	privateKey, err := pemutil.ParseOpenSSHPrivateKey(buf.Bytes())
+	if err != nil {
+		panic(err)
+	}
+	privateKeyChan <- privateKey.(ed25519.PrivateKey)
 }
 
 func main() {
+	if os.Getenv(ChildEnvName) != "" {
+		childProcess()
+		return
+	}
+
 	log.Println("start")
 
-	privateKeyChan := make(chan ed25519.PrivateKey)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	privateKeyChan := make(chan ed25519.PrivateKey, runtime.NumCPU())
+	var wg sync.WaitGroup
 	for i := 0; i < runtime.NumCPU(); i++ {
-		go bruteAuthorizedKey(privateKeyChan, i, runtime.NumCPU())
+		i := i
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			child(ctx, privateKeyChan, i, runtime.NumCPU())
+		}()
 	}
 
 	privateKey := <-privateKeyChan
+	cancel()
+	wg.Wait()
 
 	log.Println("found")
 
